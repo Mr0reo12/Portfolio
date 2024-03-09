e2f35d8a17f0caaa42d7d9ef1ba4aba417e7a808
Mr0reo12 <angelo321.lagomarsino123@gmail.com>
2024-03-09 18:04:49 +0100

    final commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,7 @@ import Inicio from '@/component/Inicio/Inicio'
 
 export default function Home() {
   return (
-    <main className="flex flex-col min-h-screen bg-custom-cream font-custom">
     <Inicio/>
-  
-    </main>
   
   )
 }
